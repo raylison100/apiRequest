66f94f95ae53e3c9b6c6088f4135859a3653d542
ralison100@ <ralison100@gmail.com>
2018-10-18 12:59:43 -0300

    curl

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 
-use \routes\Router;
-
-require_once 'Router.php';
-
-$app = new Router();
-$bucaController = new BuscaController();
-
-$app->route('/get', function(){
-    return "iuyfsdiu";
-});
-
-$app->route('/post', function(){
-    echo "pagina de contato!";
-});
-
-$app->run();
+//url solicitada
+//$url = "http://10.1.1.219:8080/psv/airports";
+$url ="http://headers.jsontest.com/";
+//$method  =  $SERVER['REQUEST_METHOD'];
+//Inicializa cURL para uma URL.
+$ch = curl_init($url);
+//Marca que vai receber string
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//Inicia a conexão
+$result = curl_exec($ch);
+//Fecha a conexão
+curl_close($ch);
+echo $result;
+echo $method;
+?>

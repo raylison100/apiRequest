63a59a037543879ab31d2bdc794fdb7b126445ef
ralison100@ <ralison100@gmail.com>
2018-10-22 11:56:17 -0300

    Realizando requisições via Guzzle e tratando os response

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'vendor/autoload.php';
+
 class BuscaController
 {
 
@@ -8,9 +10,12 @@ class BuscaController
 
 
         switch ($url["path"]) {
-            case '/voos':
+            case '/airports':
 
-                $url2 = "https://gateway.buscaaereo.com.br/psv/airports";
+                $url2 = "https://gateway.buscaaereo.com.br/psv/";
+
+                /*//Ultilizando biblioteca Curl
+
                 //Inicializa cURL para uma URL.
                 $ch = curl_init($url2);
                 //Marca que vai receber string
@@ -19,10 +24,45 @@ class BuscaController
                 $result = curl_exec($ch);
                 //Fecha a conexão
                 curl_close($ch);
+                echo $result;*/
 
-                echo $result;
 
+                //Ultilizando cliente HTTP Guzzle
+
+                try {
+                    // Cria um cliente com uma base URI
+                    $client = new  GuzzleHttp \ Client (['base_uri' => $url2]);
+                    // Envie um pedido para "https://gateway.buscaaereo.com.br/psv/airports"
+                    $response = $client->request('GET', 'airports');
+
+                    /*// outra opção
+                    $client = new GuzzleHttp\Client();
+                    $response  =  $client -> request ( 'GET' ,  'https://gateway.buscaaereo.com.br/psv/airports' );*/
+
+                } catch (RequestException  $e) {
+                    //(tempo limite de conexão, erros de DNS, etc.)
+                    echo Psr7 \ str($e->getRequest());
+                    if ($e->hasResponse()) {
+                        echo Psr7 \ str($e->getResponse());
+                    }
+
+                } catch (ClientException  $e) {
+                    //Lançado para erros de nível 400
+                    echo Psr7 \ str($e->getRequest());
+                    echo Psr7 \ str($e->getResponse());
+
+                } catch (ServerException $e){
+                    //Lançado para erros de nível 500
+                    echo Psr7 \ str($e->getRequest());
+                    echo Psr7 \ str($e->getResponse());
+                }
+
+                if ($response->getStatusCode() == '200') {
+                    echo $response->getBody();
+                } else
+                    echo 'Sistema indisponivel';
                 break;
+
             default:
 
                 echo 'ROTA NÂO PERMITIDA' . "\n";
@@ -77,7 +117,7 @@ class BuscaController
     {
         if ($this->validaRequicao()) {
             $this->rederect($this->quebraURL());
-        }else
+        } else
             echo "Requição não permitida";
 
     }

f4f0ac00b710a94c8dd7438fb36daab82c6d38a4
ralison100@ <ralison100@gmail.com>
2018-10-31 10:38:57 -0300

    Ativação do controller de email

--- SRC/Controller/EmailController.php
+++ SRC/Controller/EmailController.php
@@ -40,7 +40,7 @@ class EmailController
     public function index()
     {
 
-        /*$this->mail->isSMTP();//Tell PHPMailer to use SMTP
+        $this->mail->isSMTP();//Tell PHPMailer to use SMTP
         $this->mail->SMTPDebug = 0; //Enable SMTP debugging 0 = off (for production use),1 = client messages, 2 = client and server messages
         $this->mail->Host = 'smtp.gmail.com'; //Set the hostname of the mail server
         $this->mail->Port = 587; //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
@@ -48,13 +48,14 @@ class EmailController
         $this->mail->SMTPAuth = true; //Whether to use SMTP authentication
         $this->mail->Username = $this->headers[Username]; //Username to use for SMTP authentication - use full email address for gmail
         $this->mail->Password = $this->headers[Password]; //Password to use for SMTP authentication
-        $this->mail->setFrom($this->headers[Sender],$this->headers[SenderName]); //Set who the message is to be sent from
+        $this->mail->setFrom($this->headers[Sender], $this->headers[SenderName]); //Set who the message is to be sent from
         $this->mail->addAddress($this->headers[Recipient], $this->headers[RecipientName]); //Set who the message is to be sent to
         $this->mail->Subject = $this->headers[Subject]; //Set the subject line
         $this->mail->IsHTML(true);
         $this->mail->Body =
             "<h1>Test 1 of PHPMailer html</h1>
-            <p> {$this->headers[Body]}</p>";
+            <p> {$this->headers[Body]}</p>
+            \"http://api.request:8000/Email.php\"";
         $this->mail->AltBody = $this->headers[AltBody]; //Replace the plain text body with one created manually
         $this->mail->ConfirmReadingTo;
         //send the message, check for errors
@@ -62,8 +63,6 @@ class EmailController
             echo "Mailer Error: " . $this->mail->ErrorInfo;
         } else {
             echo "Message sent!";
-        }*/
-
-        echo "http://api.request:8000/Email.php";
+        }
     }
 }
